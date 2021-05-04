5f17e586e0db2b6d2f039639e728c7d6e45217f7
Kira Michelle Achen <kira0698@zbc.dk>
2021-05-04 15:55:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/selfieapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.selfieapp;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Here is our objects
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageview);
+        button = findViewById(R.id.button);
+
+        // Here we requests the camera for runtime permission
+        // The checkSelfPermission is the one that determine whether you have been granted a particular permission.
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 100);
+        }
+
+        //The intent to start.
+        //MediaStore is the contract between the media provider and applications, and
+        //What we ask for it here is a standard Intent action that can be sent to have the camera application capture an image and return it.
+        // oh btw we also set the button object that is set on clicklistener for open camera
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 100);
+        });
+    }
+
+    //requestCode does so that onActivityResult() when the activity exits code will be returned.
+    //we also sets a Bitmap as the content of our ImageView.
+    @Override
+    protected void onActivityResult(int requestcode, int resultcode, @Nullable Intent data) {
+        super.onActivityResult(requestcode, resultcode, data);
+        if (requestcode == 100) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+}
